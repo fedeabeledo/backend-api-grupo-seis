cada1658fa920bbd752a3144e8900566c53c5da9
pepi <fedeabeledo01@gmail.com>
2024-05-15 12:49:42 -0300

    first commit

---
+++ patitas_peludas/src/main/java/edu/uade/patitas_peludas/PatitasPeludasApplication.java
@@ -0,0 +1,13 @@
+package edu.uade.patitas_peludas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatitasPeludasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatitasPeludasApplication.class, args);
+	}
+
+}
---
+++ patitas_peludas/src/test/java/edu/uade/patitas_peludas/PatitasPeludasApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.uade.patitas_peludas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatitasPeludasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
